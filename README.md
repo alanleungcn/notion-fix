f464d1a1d945b6b2742cfb837c778ccc628e5872
Alan <alanleung1909@gmail.com>
2021-08-11 11:31:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Notion Fix
+
+A userscript to fix the follow bugs in Notion
+
+# Installation
+
+[Greasy Frok](https://greasyfork.org/en/scripts/)
+
+# Fixed bugs
+
+- Timeline view table bottom divider position
+- Scrollbar stuck to rightmost when switching from timeline view
+
+# Notion version
+
+2.11.23.9.0.39
---
+++ index.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         Notion Fix
+// @namespace    https://github.com/alanleungcn/notion-fix
+// @version      1.0
+// @author       Alan Leung
+// @description  Fix bugs in Notion
+// @match        https://www.notion.so/*
+// @license      MIT
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	let preView = null;
+	let curView = null;
+	const checkInterval = 100;
+	const viewList = [
+		'table',
+		'board',
+		'timeline',
+		'calendar',
+		'gallery',
+		'list',
+	];
+
+	const init = setInterval(() => {
+		queryView();
+		if (curView) {
+			clearInterval(init);
+			applyFix();
+		}
+	}, checkInterval);
+
+	window.addEventListener('click', () => {
+		applyFix();
+	});
+
+	function fixTimelineDividerPosition() {
+		document.querySelector('.pseudoSelection').style.position = '';
+	}
+
+	function fixScrollbarPosition() {
+		document.querySelector(
+			'.notion-scroller.vertical.horizontal'
+		).scrollLeft = 0;
+	}
+
+	function queryView() {
+		viewList.forEach((view, i) => {
+			if (document.querySelector(`.notion-${view}-view`)) {
+				curView = viewList[i];
+			}
+		});
+	}
+
+	function applyFix() {
+		requestAnimationFrame(() => {
+			queryView();
+			if (curView === 'timeline') {
+				fixTimelineDividerPosition();
+			}
+			if (curView !== preView && preView === 'timeline') {
+				fixScrollbarPosition();
+			}
+			preView = curView;
+		});
+	}
+})();
